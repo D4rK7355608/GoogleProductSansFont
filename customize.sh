c3c84b27663c91f21a46f485cad80f6be549fa6e
D4rK <61864357+D4rK7355608@users.noreply.github.com>
2022-07-19 10:42:30 +0300

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,21 @@
+# Replace Roboto to Open Sans
+REPLACE="
+/system/fonts/Roboto-Black.ttf
+/system/fonts/Roboto-BlackItalic.ttf
+/system/fonts/Roboto-Bold.ttf
+/system/fonts/Roboto-BoldItalic.ttf
+/system/fonts/RobotoCondensed-Bold.ttf
+/system/fonts/RobotoCondensed-BoldItalic.ttf
+/system/fonts/RobotoCondensed-Italic.ttf
+/system/fonts/RobotoCondensed-Light.ttf
+/system/fonts/RobotoCondensed-LightItalic.ttf
+/system/fonts/RobotoCondensed-Regular.ttf
+/system/fonts/Roboto-Italic.ttf
+/system/fonts/Roboto-Light.ttf
+/system/fonts/Roboto-LightItalic.ttf
+/system/fonts/Roboto-Medium.ttf
+/system/fonts/Roboto-MediumItalic.ttf
+/system/fonts/Roboto-Regular.ttf
+/system/fonts/Roboto-Thin.ttf
+/system/fonts/Roboto-ThinItalic.ttf
+"
